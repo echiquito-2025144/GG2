aa4adacb4108780cabd6521662448815b269c479
echiquito-2025144 <echiquito-2025144@kinal.edu.gt>
2026-08-25 13:33:19 -0600

    refactor: se editó dashboard.html

--- GestionGastos/frontend/src/app/modules/dashboard/pages/dashboard/dashboard.ts
+++ GestionGastos/frontend/src/app/modules/dashboard/pages/dashboard/dashboard.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
@@ -15,6 +15,12 @@ export class DashboardComponent implements OnInit {
   private router = inject(Router);
 
   usuario: any = null;
+
+  // Valores dinámicos para las tarjetas KPI
+  saldoActual: number = 10.00;
+  ingresoMes: number = 10.00;
+  gastosMes: number = 10.00;
+  totalAhorro: number = 10.00;
 
   ngOnInit(): void {
     this.usuario = this.authService.obtenerUsuario();
